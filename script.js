145bc9eb7913dde3d5e9f0c192556d49304181e5
Jai Dyelle Dela Cruz <Jaidyelle12@gmail.com>
2024-07-19 04:22:27 +0800

    flippin

--- script.js
+++ script.js
@@ -6,5 +6,17 @@ for (let i = 0; i < emo.length; i++){
     let box = document.createElement('div');
     box.className = "content";
     box. innerHTML = shuffle[i];
+
+
+    box.addEventListener('click', event =>{
+        box.classList.add('boxOpen');
+
+        if(box.innerHTML == "💣"){
+            setTimeout(() =>{
+                alert("Game Over");
+            }, 200)
+        }
+    })
+
     document.querySelector('.inside').appendChild(box);
 }
